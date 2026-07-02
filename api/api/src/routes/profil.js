9cfbaa6e849f07cb6ce3133dad48475286aa8208
Canaille15 <olivebeff@gmail.com>
2026-07-02 22:53:57 +0200

    fix: route PUT profil/:cp/pin ajoutee

--- api/api/src/routes/profil.js
+++ api/api/src/routes/profil.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const c = require('../controllers/profilController');
 const { authMiddleware } = require('../middleware/auth');
-
 router.get('/:cp',                   authMiddleware, c.getProfil);
 router.put('/:cp',                   authMiddleware, c.updateProfil);
 router.post('/:cp/roulement',        authMiddleware, c.setRoulement);
 router.get('/:cp/roulement/actif',   authMiddleware, c.getRoulementActif);
 router.put('/:cp/habilitations',     authMiddleware, c.setHabilitations);
 router.post('/:cp/famille',          authMiddleware, c.addFamille);
-
+router.put('/:cp/pin',               authMiddleware, c.resetPin);
 module.exports = router;
